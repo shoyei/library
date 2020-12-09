c695657dcff02ca65b96de01fa3be1fcac50b3a0
shoyei <zacsrogers@gmail.com>
2020-12-09 10:41:48 -0800

    main functions complete

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ let inputAuthor = document.getElementById('inputAuthor')
 let inputPages = document.getElementById('inputPages')
 let inputRead = document.getElementById('inputRead')
 let inputBtn = document.querySelector('#enter')
+let updateReadBtn = document.querySelector('.read');
 let books = document.querySelector('.book')
-let deleteBtn = document.getElementById('')
+let deleteBtn = document.getElementsByClassName('delete')
 let bookID = 0
 
 //Object array
@@ -19,21 +20,21 @@ let myLibrary = [
         author: 'John Smith',
         pages: 500,
         read: true,
-        
+
     },
     {
         title: 'My Tale',
         author: 'Jane Doe',
         pages: 300,
         read: false,
-        
+
     },
     {
         title: 'Sci-Fi',
         author: 'Glaxt',
         pages: 300,
         read: false,
-        
+
     }
 ]
 
@@ -53,15 +54,17 @@ function addToLibrary(_title, _author, _pages, _read) {
 
 //Create page content
 function createBook(i) {
-    const _currentBookID = bookID.toString()
+    const _currentBookID = bookID
     let _bookDiv = document.createElement('div');
     _bookDiv.classList.add('book');
-    _bookDiv.setAttribute("id", 'book'+_currentBookID);
+    _bookDiv.setAttribute("id", _currentBookID);
 
     let _delDiv = document.createElement('div');
-    _delDiv.setAttribute('id', 'delete'+_currentBookID);
+    _delDiv.classList.add('delete')
+    _delDiv.setAttribute('id', 'delete' + _currentBookID);
     _delDiv.textContent = 'Delete';
     _bookDiv.appendChild(_delDiv);
+    _delDiv.addEventListener('click', removeBook)
 
     let _titleDiv = document.createElement('div');
     _titleDiv.classList.add('label');
@@ -69,7 +72,7 @@ function createBook(i) {
 
     let _titleDetailDiv = document.createElement('div');
     _titleDetailDiv.classList.add('detail');
-    _titleDetailDiv.textContent = `${myLibrary[_currentBookID].title}`
+    _titleDetailDiv.textContent = `${myLibrary[_currentBookID].title}`;
     _titleDiv.appendChild(_titleDetailDiv);
     _bookDiv.appendChild(_titleDiv);
 
@@ -79,7 +82,7 @@ function createBook(i) {
 
     let _authorDetailDiv = document.createElement('div');
     _authorDetailDiv.classList.add('detail');
-    _authorDetailDiv.textContent = `${myLibrary[_currentBookID].author}`
+    _authorDetailDiv.textContent = `${myLibrary[_currentBookID].author}`;
     _authorDiv.appendChild(_authorDetailDiv);
     _bookDiv.appendChild(_authorDiv);
 
@@ -89,32 +92,45 @@ function createBook(i) {
 
     let _pagesDetailDiv = document.createElement('div');
     _pagesDetailDiv.classList.add('detail');
-    _pagesDetailDiv.textContent = `${myLibrary[_currentBookID].pages}`
+    _pagesDetailDiv.textContent = `${myLibrary[_currentBookID].pages}`;
     _pagesDiv.appendChild(_pagesDetailDiv);
     _bookDiv.appendChild(_pagesDiv);
 
     let _readDiv = document.createElement('div');
     _readDiv.classList.add('label');
     _readDiv.textContent = "Read?";
-    
-    let _readDetailDiv = document.createElement('div');
-    _readDetailDiv.classList.add('detail');
 
-    if (myLibrary[bookID].read === true){
+    let _readDetailDiv = document.createElement('div');
+    _readDetailDiv.classList.add('read');
+
+    if (myLibrary[bookID].read === true) {
         _readDetailDiv.innerHTML = '<input type="checkbox" id="readcheck" checked>';
     }
-    else {
+    if (myLibrary[bookID].read === false) {
         _readDetailDiv.innerHTML = '<input type="checkbox" id="readcheck">';
     }
 
+    _readDetailDiv.addEventListener('click', updateRead);
     _readDiv.appendChild(_readDetailDiv);
-
     _bookDiv.appendChild(_readDiv);
-
-    myLibrary[bookID].bookID = _currentBookID;
     mainDiv.appendChild(_bookDiv);
     bookID += 1;
+    console.log(myLibrary);
 
+}
+
+function removeBook(e) {
+    // find index of book where the selected 'delete' button is located.
+    let removeIndex = e.target.parentNode.getAttribute('id');
+    // remove that index from library
+    myLibrary.splice(removeIndex, 1);
+    bookID -= 1;
+    applyID();
+    // remove book from frontend
+    let _book = e.target.parentElement;
+    mainDiv.removeChild(_book);
+
+    console.log(myLibrary);
 }
 
 //Populate page with pre-existing objects
@@ -122,16 +138,32 @@ function addToDisplay() {
     for (i = 0; i <= myLibrary.length; i++) {
         createBook();
     }
+    applyID();
 }
-
 
 function newToDisplay() {
     createBook();
+    console.log(myLibrary);
 }
 
-function deleteBook(_id) {
-    let _div = document.querySelector(`#delete${_id}`)
-    mainDiv.removeChild(_div)
+function updateRead(e) {
+    let updateIndex = e.target.parentNode.parentNode.parentNode.getAttribute('id');
+    if (myLibrary[updateIndex].read == true) {
+        myLibrary[updateIndex].read = false;
+        console.log(myLibrary)
+    }
+    if (myLibrary[updateIndex].read == false) {
+        myLibrary[updateIndex].read = true;
+        console.log(myLibrary)
+    }
+    else console.log('Error')
+    console.log(myLibrary)
+}
+
+function applyID() {
+    for (i = 0; i < myLibrary.length; i++) {
+        myLibrary[i].bookID = i;
+    }
 }
 
 function overlayOn() {
@@ -153,20 +185,28 @@ plusBtn.addEventListener('click', function () {
 
 inputExit.addEventListener('click', function () {
     overlayOff();
+    inputTitle.value = "";
+    inputAuthor.value = "";
+    inputPages.value = "";
 }, false)
 
-inputRead.addEventListener('change', function (){
-    if (myLibrary[_currentBookID].read.value === false) {
-    myLibrary[_currentBookID].read.value = true;
-    }
-    if (myLibrary[_currentBookID].read.value === true) {
-        myLibrary[_currentBookID].read.value = false;
-    }
-})
+// inputRead.addEventListener('change', function (){
+//     if (myLibrary[_currentBookID].read.value === false) {
+//     myLibrary[_currentBookID].read.value = true;
+//     }
+//     if (myLibrary[_currentBookID].read.value === true) {
+//         myLibrary[_currentBookID].read.value = false;
+//     }
+// })
 
 inputBtn.addEventListener('click', function () {
+    // if (inputTitle.value ==="") {
+
+    //     return;
+    // }
+    // if (inputAuthor.value === "") return;
     addToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
-    console.log(myLibrary)
+    console.log(myLibrary);
     newToDisplay();
     overlayOff();
 })
